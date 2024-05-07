94752919ab771d53d2a4bb1a5b93d345a2c50f04
BlueZoft <akilaariyathilaka@gmail.com>
2024-05-07 20:31:37 +0100

    testing halfway

--- __tests__/Render.test.js
+++ __tests__/Render.test.js
@@ -1,19 +1,13 @@
-import { render, screen } from '@testing-library/react'
+import { render, screen, cleanup } from '@testing-library/react'
 import '@testing-library/jest-dom'
 import AboutPage from '../src/app/about/page.js'
-import ProductsPage from '../src/app/products/page.js'
 
+afterEach(() => {
+  cleanup();
+});
 
-test('AboutPage renders correctly', () => {
+test('Pages renders correctly', () => {
   render(<AboutPage />);
   const titleElement = screen.getByText(/Crunch, Crackle, Crumbl/i)
   expect(titleElement).toBeInTheDocument()
-
-});
-
-test('Products Page renders correctly', () => {
-  render(<ProductsPage />);
-  const buttonElement = screen.getByText(/Crunch, Crackle, Crumbl/i)
-  expect(buttonElement).toBeInTheDocument();
-
 });
