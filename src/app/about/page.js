64c490a7318e505a6e33e09026ab3d4d4c2bbfdc
BlueZoft <akilaariyathilaka@gmail.com>
2024-05-09 21:23:10 +0100

    test run 2

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -12,14 +12,18 @@ const AboutPage = () => {
   return (
     <div className={styles.container}>
       <div className={styles.textContainer}>
-        <h2 className={styles.subtitle}>Kilmaine Kakes</h2>
+
         <h1 className={styles.title}>
         Crunch, Crackle, Crumble - The Luscious Sounds of Kilmaine Kakes
         </h1>
         <p className={styles.desc}>
         At Kilmaine Kakes, the bakers are constantly exploring new flavor combinations and creative twists on traditional recipes, daring to push the boundaries of what a bakery can offer. Their culinary artisans let their imaginations run wild, sizzling up innovative treats that tantalize the taste buds with each bite.
-However, amidst their whirlwind of inventiveness, Kilmaine Kakes remains grounded in the time-honored recipes that have made them a beloved institution. Their master bakers meticulously craft each batch, ensuring every classic offering adheres to the highest standards of quality and authenticity.
-From the gentle whoosh of whisks folding in rich, velvety batters to the soothing sizzle of fragrant spices being toasted to perfection, the bakery hums with the harmonious melodies of culinary creation. It is this delicate balance of cherishing tradition while blazing new trails that makes Kilmaine Kakes a truly unique and unforgettable experience for sweet aficionados near and far.
+However, amidst their whirlwind of inventiveness, Kilmaine Kakes remains grounded in the time-honored recipes that have made them a beloved institution.
+</p>
+<p className={styles.desc}>
+Come meet us! - Castle Park Rd, Bangor BT20 4TD
+<br></br>
+Talk to us! - 0345 600 7555
         </p>
         <div className={styles.boxes}>
           <div className={styles.box}>
